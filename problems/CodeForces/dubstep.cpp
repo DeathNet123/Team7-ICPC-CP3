e2a39f22204852d7f488e7f02f37db6df5a5ecaa
DeathNet123 <arslan.computer2002@gmail.com>
2022-01-21 20:16:18 +0500

    dumbsteps added

--- problems/CodeForces/dubstep.cpp
+++ problems/CodeForces/dubstep.cpp
@@ -1,17 +1,31 @@
 #include<iostream>
 #include<algorithm>
 #include<string>
+#include<regex>
 using namespace std;
 int main(void)
 {
-    string cake;
-    string song {""};
-    string temp;
-    int count = 0;
-    cin>>cake;
-    for(char c : cake)
+    string input;
+    cin>>input;
+    regex reg("WUB");
+    string news = regex_replace(input, reg, "-");
+    regex last("-+");
+    string output = regex_replace(news, last, " ");
+    char las = output[output.length()-1];
+    char first = output[0];
+    if(las == ' ')
+        cout<<output.substr(0, output.length()-1);
+    else if (first == ' ' && las == ' ')
     {
-        
+        cout<<output.substr(1, output.length());
+    }
+    else if(first == ' ')
+    {
+        cout<<output.substr(1, output.length())<<endl;
+    }
+    else
+    {
+        cout<<output<<endl;
     }
     return 0;
 }
