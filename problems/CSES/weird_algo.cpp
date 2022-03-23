f6eccbd69f534e06b541eedceaa4d0e7eabbd752
DeathNet123 <arslan.computer2002@gmail.com>
2022-03-23 19:22:54 +0500

    weird algo

---
+++ problems/CSES/weird_algo.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(void)
+{
+    long long n{138367};
+    while(n != 1)
+    {
+        cout << n<<" ";
+        if(n % 2 == 0)
+        {
+            n /= 2;
+            continue;
+        }
+        else
+        {
+            n *= 3;
+            n += 1;
+        }
+    }
+    cout<<n;
+    return 0;
+}
